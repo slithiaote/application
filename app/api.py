bb6c9a38ea0a42f9521d79c25342b800c2d918cb
slithiaote <sebastien.li-thiao-te@ensae.fr>
2026-03-17 09:21:04 +0000

    use trained model from mlflow

--- app/api.py
+++ app/api.py
@@ -1,17 +1,41 @@
 """A simple API to expose our trained RandomForest model for Tutanic survival."""
 
+import logging
 from fastapi import FastAPI
-import skops.io as sio
 import pandas as pd
+import mlflow
 
-unknown_types = sio.get_untrusted_types(file="model.skops")
-model = sio.load("model.skops", trusted=unknown_types)
+
+logging.basicConfig(
+    format="{asctime} - {levelname} - {message}",
+    style="{",
+    datefmt="%Y-%m-%d %H:%M",
+    level=logging.DEBUG,
+    handlers=[logging.FileHandler("api.log"), logging.StreamHandler()],
+)
+
+# Preload model -------------------
+
+logging.info(
+    "Getting model from MLFlow"
+)
+
+model_name = "production"
+model_version = "latest"
+
+# Load the model from the Model Registry
+model_uri = f"models:/{model_name}/{model_version}"
+model = mlflow.sklearn.load_model(model_uri)
+
+# Define app -------------------------
+
 
 app = FastAPI(
     title="Prédiction de survie sur le Titanic",
-    description="Application de prédiction de survie sur le Titanic 🚢 v0.0.1 <br>Une version par API pour faciliter la réutilisation du modèle 🚀"
-    + '<br><br><img src="https://media.vogue.fr/photos/5faac06d39c5194ff9752ec9/1:1/w_2404,h_2404,c_limit/076_CHL_126884.jpg" width="200">',
-)
+    description=
+    "Application de prédiction de survie sur le Titanic 🚢 <br>Une version par API pour faciliter la réutilisation du modèle 🚀" +\
+        "<br><br><img src=\"https://media.vogue.fr/photos/5faac06d39c5194ff9752ec9/1:1/w_2404,h_2404,c_limit/076_CHL_126884.jpg\" width=\"200\">"
+    )
 
 
 @app.get("/", tags=["Welcome"])
@@ -22,16 +46,20 @@ def show_welcome_page():
 
     return {
         "Message": "API de prédiction de survie sur le Titanic",
-        "Model_name": "Titanic ML",
-        "Model_version": "0.1",
+        "Model_name": 'Titanic ML',
+        "Model_version": "0.3",
     }
 
 
 @app.get("/predict", tags=["Predict"])
 async def predict(
-    sex: str = "female", age: float = 29.0, fare: float = 16.5, embarked: str = "S"
+    sex: str = "female",
+    age: float = 29.0,
+    fare: float = 16.5,
+    embarked: str = "S"
 ) -> str:
-    """ """
+    """
+    """
 
     df = pd.DataFrame(
         {
@@ -43,6 +71,8 @@ async def predict(
     )
 
     prediction = int(model.predict(df)[0])
+
+    logging.debug(f"Predicting {prediction} for {df.to_dict()}")
 
     prediction = "Survived 🎉" if prediction == 1 else "Dead ⚰️"
 
--- app/run.sh
+++ app/run.sh
@@ -1,2 +1,1 @@
-uv run train.py
 uv run uvicorn app.api:app --host "0.0.0.0"
